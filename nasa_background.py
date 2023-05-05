8b8fcf0d3cf43c83cf9ebfe1c1636c7c82902e50
Philipp Holz <philipp.holz@esentri.com>
2023-05-05 03:49:32 +0200

    adds windows and macOS switch, ignore image on vcs

--- nasa_background.py
+++ nasa_background.py
@@ -2,6 +2,7 @@ import requests
 import subprocess
 import os
 import logging
+import platform
 
 # Set up logging
 logging.basicConfig(filename='nasa_background.log', level=logging.DEBUG)
@@ -20,10 +21,17 @@ with open('nasa_image.jpg', 'wb') as f:
 
 # Set desktop background
 try:
-
-    file_location = os.path.abspath("nasa_image.jpg")
-    osascript = f'tell application "System Events" to set picture of every desktop to "{file_location}"'
-    os.system(f"osascript -e '{osascript}'")
+    if platform.system() == 'Darwin':  # macOS
+        file_location = os.path.abspath("nasa_image.jpg")
+        osascript = f'tell application "System Events" to set picture of every desktop to "{file_location}"'
+        os.system(f"osascript -e '{osascript}'")
+    elif platform.system() == 'Windows':  # Windows
+        import ctypes
+        SPI_SETDESKWALLPAPER = 20
+        file_location = os.path.abspath("nasa_image.jpg")
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, file_location, 0)
+    else:
+        logging.error('Unsupported operating system: %s' % platform.system())
 
     logging.info('Desktop background set successfully.')
 except subprocess.CalledProcessError as e:
